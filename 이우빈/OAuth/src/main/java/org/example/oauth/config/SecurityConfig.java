b345496481bddc27959e990de12ce4ecbb0fca02
0xwb7 <woobin200111@gmail.com>
2025-11-15 14:31:11 +0900

    feat: TokenDto 생성

--- 이우빈/OAuth/src/main/java/org/example/oauth/config/SecurityConfig.java
+++ 이우빈/OAuth/src/main/java/org/example/oauth/config/SecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity(debug = true)
