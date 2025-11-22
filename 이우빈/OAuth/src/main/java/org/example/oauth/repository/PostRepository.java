0ef87ef20f3a3e7707c1ac35e814c1f349388716
0xwb7 <woobin200111@gmail.com>
2025-11-22 18:52:14 +0900

    feat: 글 목록 출력에서 발생하는 N+1 문제를 해결하기 위한 Entity 어노테이션 추가

--- 이우빈/OAuth/src/main/java/org/example/oauth/repository/PostRepository.java
+++ 이우빈/OAuth/src/main/java/org/example/oauth/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package org.example.oauth.repository;
 
 import org.example.oauth.domain.post.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @EntityGraph(attributePaths = "author")
+    List<Post> findAll();
 }
